dc9e0038a637e6030ed13fcc49c7fc0a81689c02
K9Gnoah <hoangdominohotboizbg@gmail.com>
2023-10-23 14:57:54 +0700

    Create UserDBContext.java in dal folder
    add method getUsers to UserDBContext.java. getUsers using sql command to querying data

---
+++ ProjectPRJ_Assigment/src/java/dal/UserDBContext.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.User;
+
+/**
+ *
+ * @author THINKPAD
+ */
+public class UserDBContext extends DBContext<User> {
+
+    @Override
+    public User getUsers(User model) {
+        try {
+            String sql = "SELECT [accountId]\n"
+                    + "      ,[username]\n"
+                    + "      ,[password]\n"
+                    + "      ,[displayname]\n"
+                    + "  FROM [dbo].[Account]\n"
+                    + "  WHERE username= ? AND [password] = ?";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setString(1, model.getUsername());
+            stm.setString(2, model.getPassword());
+            ResultSet rs = stm.executeQuery();
+            if (rs.next()) {
+                User user = new User();
+                user.setDisplayname(rs.getString("displayname"));
+                user.setPassword(model.getPassword());
+                user.setUsername(model.getUsername());
+                return user;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+   
+}
