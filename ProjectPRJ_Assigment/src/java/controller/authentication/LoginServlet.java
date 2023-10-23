564b5fe8dc20a0c686d492843eaeeece1dd8b9e9
K9Gnoah <hoangdominohotboizbg@gmail.com>
2023-10-23 15:00:41 +0700

    Update LoginServlet.java
    get parameter user, pass and check if the user exist in database to login

--- ProjectPRJ_Assigment/src/java/controller/authentication/LoginServlet.java
+++ ProjectPRJ_Assigment/src/java/controller/authentication/LoginServlet.java
@@ -5,12 +5,17 @@
 
 package controller.authentication;
 
+import dal.UserDBContext;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.util.ArrayList;
+import model.User;
 
 /**
  *
@@ -55,24 +60,28 @@ public class LoginServlet extends HttpServlet {
     throws ServletException, IOException {
         request.getRequestDispatcher("view/login.jsp").forward(request, response);
     } 
-
-    /** 
-     * Handles the HTTP <code>POST</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+   
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        processRequest(request, response);
+    throws ServletException, IOException {        
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        User param = new User();
+        param.setUsername(username);
+        param.setPassword(password);
+        UserDBContext db = new UserDBContext();
+        User loggedUser = db.getUsers(param);
+        if (loggedUser != null) {
+            HttpSession session = request.getSession();
+            session.setAttribute("account", loggedUser);                    
+            response.getWriter().println("Hello " + loggedUser.getDisplayname());
+        } else {
+            response.getWriter().println("invalid username or password!");
+        }
+
     }
 
-    /** 
-     * Returns a short description of the servlet.
-     * @return a String containing servlet description
-     */
+    
     @Override
     public String getServletInfo() {
         return "Short description";
