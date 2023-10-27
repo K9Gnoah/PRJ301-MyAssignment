b2127331a5f1994d86290c9dfb15866c87bb5390
K9Gnoah <hoangdominohotboizbg@gmail.com>
2023-10-27 23:50:09 +0700

    Update Subject.java
    implement IBaseModel, add setter

--- ProjectPRJ_Assigment/src/java/model/Subject.java
+++ ProjectPRJ_Assigment/src/java/model/Subject.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
  *
  * @author THINKPAD
  */
-public class Subject {
+public class Subject implements IBaseModel{
     private int id;
     private String name;
     private ArrayList<Group> groups = new ArrayList<>();
