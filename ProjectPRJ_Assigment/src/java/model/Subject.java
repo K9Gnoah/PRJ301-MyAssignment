c0a60e1a73ff45723e0acfdeec281bee8bab1ead
K9Gnoah <hoangdominohotboizbg@gmail.com>
2023-10-27 13:37:23 +0700

    Create Subject.java

---
+++ ProjectPRJ_Assigment/src/java/model/Subject.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author THINKPAD
+ */
+public class Subject {
+    private int id;
+    private String name;
+    private ArrayList<Group> groups = new ArrayList<>();
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Group> getGroups() {
+        return groups;
+    }
+
+    
+}
