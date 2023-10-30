9fb8a9e9f6b068fb783dc172a25ecd7a21f3501e
K9Gnoah <hoangdominohotboizbg@gmail.com>
2023-10-30 14:42:16 +0700

    Update TimeTableController.java
    create a list week that have all days of the current week, get monday, get sunday

--- ProjectPRJ_Assigment/src/java/controller/admin/schedule/TimeTableController.java
+++ ProjectPRJ_Assigment/src/java/controller/admin/schedule/TimeTableController.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller.admin.schedule;
 
 import controller.authentication.BasedRequiredAuthentication;
@@ -23,24 +22,26 @@ import java.util.ArrayList;
  * @author THINKPAD
  */
 public class TimeTableController extends BasedRequiredAuthentication {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");       
-                
-        
-    } 
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -48,29 +49,41 @@ public class TimeTableController extends BasedRequiredAuthentication {
      */
     @Override
     protected void processGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         //set date 
         String date_raw = request.getParameter("date");
         LocalDate date;
-        if(date_raw == null){
+        if (date_raw == null) {
             date = LocalDate.now();
-        }else{
+        } else {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
             date = LocalDate.parse(date_raw, formatter);
         }
-        
+
         request.setAttribute("date", date);
         LocalDate monday = date;
         //check if variable name monday is not monday
-        while(monday.getDayOfWeek() != DayOfWeek.MONDAY){
+        while (monday.getDayOfWeek() != DayOfWeek.MONDAY) {
             monday = monday.minusDays(1);
         }
         //Monday
-        Date sqlMon = Date.valueOf(monday);        
-        } 
+        Date sqlMon = Date.valueOf(monday);
 
-    /** 
+        //array list days of current week
+        ArrayList<Date> week = new ArrayList<>();
+        for (int i = 0; i < 7; i++) {
+            week.add(Date.valueOf(monday.plusDays(i)));
+        }
+        request.setAttribute("week", week);
+        //Sunday
+        Date sqlSun = week.get(6);
+        
+        
+    }
+
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -78,12 +91,13 @@ public class TimeTableController extends BasedRequiredAuthentication {
      */
     @Override
     protected void processPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
